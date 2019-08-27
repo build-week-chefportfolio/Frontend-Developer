62737e58e351989d8268ca39e495bf57359e0e9a
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-08-27 18:58:21 -0400

    added routing and created navbar

---
+++ bohcura/src/components/Recipe.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Recipe () {
+    return (
+        <div>Hello Recipe</div>
+    )
+}
--- bohcura/src/index.js
+++ bohcura/src/index.js
@@ -8,13 +8,18 @@ import thunk from 'redux-thunk';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { reducer } from './reducers';
 import './App.css';
-import SignUp from "./components/Signup";
-
 
 import Confirmation from "./components/forms/onboarding/Confirmation";
 import Personal from "./components/forms/onboarding/Personal";
 import Signup from "./components/Signup";
+import SignIn from "./components/Signin";
 import HomePage from "./components/HomePage";
+import Nav from "./components/Nav";
+import Onboarding from "./components/forms/onboarding/Onboarding";
+import Feed from "./components/feed/Feed";
+import Dashboard from "./components/Dashboard";
+import Profile from "./components/Profile";
+import Recipe from "./components/Recipe";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -24,12 +29,16 @@ const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      <Signup />
-      <Personal />
-      <Confirmation />
+      <Nav />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/signup" component={Onboarding} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/feed" component={Feed} />
+      <Route path="/profile/:id" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/recipe/:id" component={Recipe} />
     </div>
-  );
+  )
 }
 
 const rootElement = document.getElementById('root');
