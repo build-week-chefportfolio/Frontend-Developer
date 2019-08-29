fd82545f66bc47869702b03e428c821a501979d5
Timothy McGowen <mcgowen17@yahoo.com>
2019-08-29 14:01:06 -0700

    Adjusted the

--- bohcura/src/components/forms/onboarding/Onboarding.js
+++ bohcura/src/components/forms/onboarding/Onboarding.js
@@ -34,6 +34,7 @@ const Onboarding = (props) => {
             // post the chef
             console.log('Case Four Activated', adjust.steps);
             props.postChef(chef);
+            (async (postChef, chef) => await postChef(chef))(props.postChef, chef)
             return (<div>{(!adjust.steps === 4) ? <div>Redirecting...</div> : props.history.push("/dashboard")}</div>)
         default:
             console.log('You are in Default and shouldnt be')
